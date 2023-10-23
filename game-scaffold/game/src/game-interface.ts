f84b9ee66cf9327f7fb1c35e66895b88a352b8f5
Joshua Hull <josh@fireflop.com>
2023-10-23 14:27:19 +0200

    Remove game scaffold

--- game-scaffold/game/src/game-interface.ts
+++
@@ -1,5 +0,0 @@
-import setup from './index';
-import { createInteface } from 'boardzilla/game/utils';
-
-const { initialState, processMove, getPlayerState } = createInteface(setup)
-export { initialState, processMove, getPlayerState };
