8fcf30f636054da47da0bb59cf11cb7a92aeadf7
Joshua Hull <josh@fireflop.com>
2024-02-23 19:25:02 +0100

    Fix readiness

--- internal/site/src/App.tsx
+++ internal/site/src/App.tsx
@@ -311,7 +311,7 @@ function App() {
         id: u.id,
         name: player?.name ?? u.name,
         avatar: avatarURL(u.id),
-        playerDetails: playerDetailsForUser(host, players, u.id, true),
+        playerDetails: playerDetailsForUser(host, players, u.id, playerReadiness.get(u.id) || false),
       })
     })
 
@@ -332,7 +332,7 @@ function App() {
     })
 
     return users
-  }, [host, numberOfUsers, players])
+  }, [host, numberOfUsers, playerReadiness, players])
 
   const processKey = useCallback((code: string): boolean => {
     const keys = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0']
